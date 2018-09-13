df36348d535ad0b6cde96ef7bec4db54fc27faa6
Gabriel Jealmo <gabrieljealmo@gmail.com>
2018-09-13 10:53:49 +0200

    Added more methods which haven't been tested out yet.

--- Soduku/SudokuMethods.cs
+++ Soduku/SudokuMethods.cs
@@ -53,12 +53,12 @@ namespace Soduku
         /**
          * Returns an array with the values in the puzzle's box.
          */
-        private static List<char> FindNumbers(int start, int stop, char[,] puzzle)
+        private static List<char> FindNumbers(int startRow, int stopRow, int startCol, int stopCol, char[,] puzzle)
         {
             List<char> boxContains = new List<char>();
-            for (int col = start; col < stop; col++)
+            for (int col = startCol; col < stopCol; col++)
             {
-                for (int row = start; row < stop; row++)
+                for (int row = startRow; row < stopRow; row++)
                 {
                     if(!puzzle[col,row].Equals(' '))
                     {
@@ -73,20 +73,53 @@ namespace Soduku
         /**
          * Returns all the valid numbers in the current 3x3 area.
          */
-        private static List<char> GetValidNumber(int x, int y, char[,] puzzle)
+        private static List<char> GetValidNumbers(int x, int y, char[,] puzzle)
         {
             List<char> boxContains = new List<char>();
+            //Ruta 0.
             if (x < 3 && y < 3)
             {
-                 boxContains = FindNumbers(0, 3, puzzle);
+                boxContains = FindNumbers(0, 3, 0, 3, puzzle);
             }
-            else if((x > 2 && x < 5) && (y > 2 && y < 5))
+            //Ruta 1,0.
+            else if (x < 3 && (y > 2 %% y < 5))
             {
-                boxContains = FindNumbers(3, 5, puzzle);
+                boxContains = FindNumbers(0, 3, 3, 5, puzzle);
             }
+            //Ruta 2,0.
+            else if(x < 3 && (y > 5 && y < 9))
+            {
+                boxContains = FindNumbers(0, 3, 5, 9, puzzle);
+            }
+            //Ruta 0,1.
+            else if ((x > 2 && x < 5) && y < 3)
+            {
+                boxContains = FindNumbers(3, 5, 0, 3, puzzle);
+            }
+            //MittenRutan 1,1.
+            else if ((x > 2 && x < 5) && (y > 2 && y < 5))
+            {
+                boxContains = FindNumbers(3, 5, 3, 5, puzzle);
+            }
+            //Ruta 2,1.
+            else if ((x > 2 && x < 5) && (y > 5 && y < 9))
+            {
+                boxContains = FindNumbers(3, 5, 5, 9, puzzle);
+            }
+            //Ruta 0,2.
+            else if((x > 5 && x < 9) && y < 3)
+            {
+                boxContains = FindNumbers(5, 9, 0, 3, puzzle);
+            }
+            //Ruta 1,2.
+            else if ((x > 5 && x < 9) && (y > 2 && y < 5))
+            {
+                boxContains = FindNumbers(5, 9, 3, 5, puzzle);
+            }
+            //Ruta 2,2.
             else
             {
-                boxContains = FindNumbers(6, 9, puzzle);
+                boxContains = FindNumbers(5, 9, 5, 9, puzzle);
             }
             return boxContains;
         }
@@ -94,23 +127,42 @@ namespace Soduku
         /**
          * Returns true if puzzle is complete.
          */
-        public void CheckIfComplete()
+        public bool CheckIfComplete(char[,] puzzle)
         {
-
+            int colLength = puzzle.GetUpperBound(0);
+            int rowLength = puzzle.GetUpperBound(1);
+            int rowTotal = 0;
+            for (int cols = 0; cols <= colLength; cols++)
+            {
+                rowTotal = 0;
+                for (int rows = 0; rows <= rowLength; rows++)
+                {
+                    rowTotal += int.Parse(puzzle[cols, rows].ToString());
+                }
+                if(rowTotal != 45)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         /**
          * Returns true/false depinding on the squares status.
          */
-        public void CheckIfValidSquare()
+        public bool CheckIfValidSquare(int x, int y, char[,] puzzle)
         {
-            for ()
+            List<char> contents = new List<char>();
+            contents = GetValidNumbers(x, y, puzzle);
+            for (int i = 1; i < 10; i++)
             {
-                for ()
+                string temp = i.ToString();
+                if (!contents.Contains(temp[0]))
                 {
-
+                    return false;
                 }
             }
+            return true;
         }
 
         /**
@@ -131,7 +183,7 @@ namespace Soduku
                 {
                     for (int rows = 0; rows <= rowLength; rows++)
                     {
-                        boxContains = GetValidNumber(rows, cols, puzzle);
+                        boxContains = GetValidNumbers(rows, cols, puzzle);
                         if (!FindInRow(start[0], cols, puzzle) && !FindInCol(start[0], rows, puzzle) && puzzle[cols, rows].Equals(' ') && !boxContains.Contains(start[0]))
                         {
                             puzzle[cols, rows] = start[0];
